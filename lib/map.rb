d38a47c145613bc61f71edea3951fd9520f62cfd
Matheus Lima <matheus.limastack@outlook.com>
2021-03-20 20:21:19 +0100

    Add valid_position method

--- lib/map.rb
+++ lib/map.rb
@@ -5,4 +5,12 @@ class Map
     @height = height
     @width = width
   end
+
+  def valid_placement?(position)
+    return false if position.nil?
+
+    (position.x >= 0 && position.x < width) && (position.y >= 0 && position.y < height)
+  end
+
+  attr_reader :width, :height
 end
