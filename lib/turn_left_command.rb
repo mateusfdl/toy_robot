a32261fa885bcedb27ef96642ac659b77eb96b44
Matheus Lima <matheus.limastack@outlook.com>
2021-04-02 20:21:19 +0100

    Add turn_left method at TurnLeftCommand

--- lib/turn_left_command.rb
+++ lib/turn_left_command.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
+# TurnLeftCommand class is responsible 
+# to turn -90 degress robot facing
 class TurnLeftCommand
+    def initialize(robot)
+        @robot = robot
+    end
+
+    def turn_left
+        return if @robot.unplaced?
+    
+        @robot.position.turn_left
+    end
 end
