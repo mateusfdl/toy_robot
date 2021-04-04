f8658346b5f67b8813ee1b90306ebbc413e8d5d9
Matheus Lima <matheus.limastack@outlook.com>
2021-04-04 20:21:19 +0100

    Add frozen_string_literal const at Spec helper config file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-core/v/3-10/docs/mock-framework-integration/mock-with-rspec
   config.mock_with :rspec
