262e38d7907ce9ab128c3c994131b1441f1d5973
Matheus Lima <matheus.limastack@outlook.com>
2021-04-04 20:21:19 +0100

    Renamed turn_left method to run

--- lib/turn_left_command.rb
+++ lib/turn_left_command.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-# TurnLeftCommand class is responsible 
+# TurnLeftCommand class is responsible
 # to turn -90 degress robot facing
 class TurnLeftCommand
-    def initialize(robot)
-        @robot = robot
-    end
+  def initialize(robot)
+    @robot = robot
+  end
 
-    def turn_left
-        return if @robot.unplaced?
-    
-        @robot.position.turn_left
-    end
+  def run
+    return if @robot.unplaced?
+
+    @robot.position.turn_left
+  end
 end
