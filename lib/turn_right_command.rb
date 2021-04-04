c26ccb4dfd006295ff400ba66fbeed5556e3934f
Matheus Lima <matheus.limastack@outlook.com>
2021-04-04 20:21:19 +0100

    Renamed turn_right method to run

--- lib/turn_right_command.rb
+++ lib/turn_right_command.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-# TurnRightCommand class is responsible 
+# TurnRightCommand class is responsible
 # to turn 90 degress robot facing
 class TurnRightCommand
-    def initialize(robot)
-        @robot = robot
-    end
+  def initialize(robot)
+    @robot = robot
+  end
 
-    def turn_right
-        return if @robot.unplaced?
-    
-        @robot.position.turn_right
-    end
+  def run
+    return if @robot.unplaced?
+
+    @robot.position.turn_right
+  end
 end
