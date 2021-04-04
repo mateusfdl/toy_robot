e5e3e3d09616a16f6affbb0c960875a0468aa835
Matheus Lima <matheus.limastack@outlook.com>
2021-04-04 20:21:19 +0100

    Add ReportCommand class

--- lib/report_command.rb
+++ lib/report_command.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
+# ReportCommand class
 class ReportCommand
+  def initialize(robot)
+    @robot = robot
+  end
+
+  def run
+    return if @robot.unplaced?
+
+    pp @robot.report
+  end
+
+  attr_reader :robot
 end
