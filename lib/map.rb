4423119ca4cff4ce1a19eca1ffceff027afaa12c
Matheus Lima <matheus.limastack@outlook.com>
2021-06-30 00:26:27 -0300

    Add Map class top Documentation

--- lib/map.rb
+++ lib/map.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Map class implements a map
 class Map
   def initialize(height, width)
     @height = height
@@ -9,7 +10,7 @@ class Map
   def valid_placement?(position)
     return false if position.nil?
 
-    (position.x >= 0 && position.x < width) && (position.y >= 0 && position.y < height)
+    (position.x >= 0 && position.x <= width) && (position.y >= 0 && position.y <= height)
   end
 
   attr_reader :width, :height
